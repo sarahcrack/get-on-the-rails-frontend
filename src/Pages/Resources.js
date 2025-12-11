1464dd9a94eae0ed38d63f02ec7b756abf618a7d
sarahcrack <73823091+sarahcrack@users.noreply.github.com>
2025-12-11 16:04:49 +0000

    returned data from backend on Resources page

--- src/Pages/Resources.js
+++ src/Pages/Resources.js
@@ -1,6 +1,26 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const API_URL = "http://localhost:3000/api/v1/resources"
+
+function getResources() {
+  return axios.get(API_URL).then((response) => response.data);
+}
+
 function Resources() {
-  return(
+  const [resources, setResources] = useState(null)
+
+  useEffect(() => {
+    getResources().then((data) => {
+      setResources(data)
+    })
+  }, [])
+
+  return (
+    <>
       <p>Hello World 🌍</p>
+      <p>{resources ? JSON.stringify(resources) : "Loading..."}</p>
+    </>
   )
 }
 
